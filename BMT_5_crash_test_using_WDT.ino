8d9c6681d039e9960bef28f85ea62640ba512aa9
ashibuna <63423793+ashibuna@users.noreply.github.com>
2020-05-01 15:42:47 -0400

    Add files via upload

---
+++ BMT_5_crash_test_using_WDT.ino
@@ -0,0 +1,20 @@
+#include <avr/wdt.h>
+ 
+void setup()
+{
+  Serial.begin(9600);
+ // wdt_enable(WDTO_1S);
+}
+ 
+void loop()
+{
+  // the program is alive...for now. 
+  //wdt_reset();
+ 
+  Serial.println("Hello");
+ 
+  while (1)
+    ; // do nothing. the program will lockup here. 
+ 
+  Serial.println("Can't get here");
+}
